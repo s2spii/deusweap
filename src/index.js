81c07de5544f082976928411ae2ce461a48a8d7a
Rayan Al Ayoubi <ralayoubi56@gmail.com>
2023-04-27 23:23:46 +0200

    Heroku deploy start

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.scss";
-import { Provider, useDispatch } from "react-redux";
+import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./reducers";
 import { getProducts } from "./actions/product.action";
